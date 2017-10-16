4a5ec4edc8c46c573105209d21637ac93a99ad22
Charly Castro <charlycastro@Charlys-MacBook-Pro.local>
2017-10-16 13:42:56 -0500

    Dockerized App

--- Frontend/config/dev.env.js
+++ Frontend/config/dev.env.js
@@ -1,7 +1,9 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
+const address = process.env.API_PORT_8000_TCP_ADDR || "localhost";
+const port = process.env.API_PORT_8000_TCP_PORT || "8000";
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:8000"'
+  API_URL: `'http://${address}:${port}'`
 })
