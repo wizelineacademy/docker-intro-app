18ba19b987400b070b9528ff0c82a7bcb8888a12
Franchesco Romero <franchesco.romero@wizeline.com>
2017-10-23 11:55:57 -0500

    formatting file

--- run-docker-compose.05.sh
+++ run-docker-compose.05.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+
 docker version
 echo "******************"
+
 docker-compose version
 echo "******************"
+
 docker-compose -f docker-compose.05.yml config
 echo "******************"
+
 docker-compose -f docker-compose.05.yml up -d
 echo "******************"
-docker ps 
+
+docker ps
 echo "******************"
