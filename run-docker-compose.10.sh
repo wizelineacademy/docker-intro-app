cd4a8cb88ab14632133ecdd93a31ca89446abe30
Franchesco Romero <franchesco.romero@wizeline.com>
2017-10-27 11:38:55 -0500

    minor fixes

--- run-docker-compose.10.sh
+++ run-docker-compose.10.sh
@@ -12,6 +12,7 @@ echo "******************"
 
 docker ps -a | awk '{ print $1,$2 }' | grep academy | awk '{print $1 }' | xargs -I {} docker stop {}
 docker ps -a | awk '{ print $1,$2 }' | grep academy | awk '{print $1 }' | xargs -I {} docker rm {}
+
 docker-compose  -f docker-compose.10.yml up -d
 echo "******************"
 
