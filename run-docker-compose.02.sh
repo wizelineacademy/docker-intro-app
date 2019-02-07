3038f1b7957d591d8687f3fab9f9eaacce052bcb
AAlvz <alfonso@alvz.mx>
2019-02-07 13:19:14 -0600

    Remove unnecesary files

--- run-docker-compose.02.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "******************"
-docker-compose -f  docker-compose-backend.02.yml -f  docker-compose-frontend.02.yml config
-echo "******************"
-docker-compose -f  docker-compose-backend.02.yml -f docker-compose-frontend.02.yml up
--- run-docker-compose.05.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "******************"
-docker ps -a | awk '{ print $1,$2 }' | grep academy | awk '{print $1 }' | xargs -I {} docker stop {}
-docker ps -a | awk '{ print $1,$2 }' | grep academy | awk '{print $1 }' | xargs -I {} docker rm {}
-docker-compose  -f docker-compose-frontend.05.yml -f docker-compose-backend.05.yml config
-docker-compose  -f docker-compose-frontend.05.yml -f docker-compose-backend.05.yml up
